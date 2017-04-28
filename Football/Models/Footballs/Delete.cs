b38399f4a21dbef6ed5078c44d41d4803ae01907
stoian1929 <stoian1929@mail.bg>
2017-04-28 23:03:17 +0300

    Update 0.1

---
+++ Football/Models/Footballs/Delete.cs
@@ -0,0 +1,25 @@
+﻿using Football.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Football.Models.Footballs
+{
+    public class Delete
+    {
+        public string TacticName { get; set; }
+
+        public string Formation { get; set; }
+
+        public PlayerPosition PlayerPosition { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+    }
+}
